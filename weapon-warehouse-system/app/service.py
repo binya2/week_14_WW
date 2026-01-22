51b775c22afa98bae6823290502b00b6f63fbe7c
Benyamin Shoraki <benyaminchor@gmail.com>
2026-01-22 12:34:35 +0200

    add error handling for CSV processing and optimize database insertion for weapons data

--- weapon-warehouse-system/app/service.py
+++ weapon-warehouse-system/app/service.py
@@ -1,30 +1,35 @@
 import io
+
 import numpy as np
 import pandas as pd
+from fastapi import UploadFile
 from numpy._core.numeric import nan
 
-from db.repository import insert_weapon
-from fastapi import UploadFile
-
+from core.errors import CSVFormatError
+from db.repository import insert_weapon, insert_weapons
 from models import WeaponsDB
 
 
 async def data_processing(file: UploadFile) -> pd.DataFrame:
-    content = await file.read()
-    csv_string = content.decode('utf-8')
-    df_weapon = pd.read_csv(io.StringIO(csv_string))
-    bins = [-1, 20, 100, 300, np.inf]
-    labels = ['low', 'medium', 'high', 'extreme']
-    df_weapon['risk_level'] = pd.cut(df_weapon['range_km'], bins=bins, labels=labels, right=True)
-    df_weapon['manufacturer'] = df_weapon['manufacturer'].replace(nan, 'Unknown')
-    return df_weapon
+    try:
+        content = await file.read()
+        csv_string = content.decode('utf-8')
+        df_weapon = pd.read_csv(io.StringIO(csv_string))
+        bins = [-1, 20, 100, 300, np.inf]
+        labels = ['low', 'medium', 'high', 'extreme']
+        df_weapon['risk_level'] = pd.cut(df_weapon['range_km'], bins=bins, labels=labels, right=True)
+        df_weapon['manufacturer'] = df_weapon['manufacturer'].replace(nan, 'Unknown')
+        return df_weapon
+    except Exception as e:
+        raise CSVFormatError(f"Failed to process CSV: {str(e)}")
 
 
 async def save_to_db(df_weapon):
     deta = df_weapon.to_dict(orient='records')
-    count = 0
-    for item in deta:
-        print(item)
-        if insert_weapon(WeaponsDB(**item)):
-            count += 1
+    list = [dic for dic in deta if insert_weapon(WeaponsDB(**dic))]
+    count = insert_weapons(list)
+    # for item in deta:
+    #     print(item)
+    #     if insert_weapon(WeaponsDB(**item)):
+    #         count += 1
     return {"status": "success", "records_added": count}
